fa911590996cf406208e6c1fd2c7fa03cf261c85
quang16499 <nguyenngocquang16499@gmail.com>
2023-11-25 21:13:56 +0700

    update log for debug

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -90,14 +90,18 @@ def authorized():
         return render_template("auth_error.html", result=request.args)
     if request.args.get('code'):
         cache = _load_cache()
-        
+        app.logger.error('currently logging.')
+
         # Acquire a token from a built msal app, along with the appropriate redirect URI
         result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
             request.args['code'],
             scopes=Config.SCOPE,
             redirect_uri=url_for('authorized', _external=True, _scheme='https')
         )
+        app.logger.error('result')
+
         if "error" in result:
+            app.logger.error('result error')
             return render_template("auth_error.html", result=result)
         session["user"] = result.get("id_token_claims")
         # Note: In a real app, we'd use the 'name' property from session["user"] below
